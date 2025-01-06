8ab0ddbd74a87644211c8815fe4c5ece541b5681
Michell Wambui Mbogo <mishie254@gmail.com>
2025-01-06 22:37:48 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// index.js
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export functions to be used in tests or other modules
+  module.exports = { findMatching, fuzzyMatch, matchName };
+  
